f216034240b77041a07f602bc6ba446134a0e9d5
Alexia Velador <alexiavel01@gmail.com>
2022-02-11 15:13:06 -0700

    new look

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h1 = document.createElement("h1");
-h1.textContent = "My Personal Website";
-document.querySelector("body").appendChild(h1);
+const h2 = document.createElement("h2");
+h2.textContent = "My Personal Website";
+document.querySelector("body").appendChild(h2);
